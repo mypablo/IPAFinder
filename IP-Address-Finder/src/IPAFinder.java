07a3b797824b46efffae48daaedbe0248044b75e
Yianni PM <51059714+mypablo@users.noreply.github.com>
2020-03-02 17:05:23 +0200

    Add files via upload

---
+++ IP-Address-Finder/src/IPAFinder.java
@@ -0,0 +1,43 @@
+import java.net.*; 
+import java.io.*; 
+import java.util.*; 
+import java.net.InetAddress; 
+import javax.swing.JOptionPane;
+
+public class IPAFinder {
+
+	 public static void main(String args[]) throws Exception 
+	    {
+		 
+		 	String pubip, sysip;
+		 	pubip = " Public IP Address: ";
+		 	sysip="System IP Address : ";
+		 
+		 
+	        //Prints System IP Address
+		 
+	        InetAddress localhost = InetAddress.getLocalHost(); 
+	        JOptionPane.showMessageDialog(null,localhost.getHostAddress(), sysip , JOptionPane.INFORMATION_MESSAGE);
+	        
+	  
+	        // Find public IP address 
+	        String systemipaddress = ""; 
+	        try
+	        { 
+	            URL url_name = new URL("http://bot.whatismyipaddress.com"); 
+	  
+	            BufferedReader sc = 
+	            new BufferedReader(new InputStreamReader(url_name.openStream())); 
+	  
+	            
+	            systemipaddress = sc.readLine().trim(); 
+	        } 
+	        catch (Exception e) 
+	        { 
+	            systemipaddress = "Cannot Execute Properly"; 
+	        } 
+	        //Prints public ip address
+	        JOptionPane.showMessageDialog(null,localhost.getHostAddress(),pubip , JOptionPane.INFORMATION_MESSAGE);
+	        System.out.println("Public IP Address: " + systemipaddress +"\n"); 
+	    } 
+	} 
